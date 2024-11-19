453c8bea046f347d38e4641a7ee2f3497a0a9dde
Le Hoan Hao <hao.le@playnext-lab.co.jp>
2024-11-20 00:11:30 +0900

    update

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,78 +1,89 @@
 export default defineAppConfig({
   ui: {
-    primary: 'green',
-    gray: 'slate',
+    primary: "green",
+    gray: "slate",
     footer: {
       bottom: {
-        left: 'text-sm text-gray-500 dark:text-gray-400',
-        wrapper: 'border-t border-gray-200 dark:border-gray-800'
-      }
-    }
+        left: "text-sm text-gray-500 dark:text-gray-400",
+        wrapper: "border-t border-gray-200 dark:border-gray-800",
+      },
+    },
   },
   seo: {
-    siteName: 'Text To Speech OpenAI API'
+    siteName: "Text To Speech OpenAI API",
   },
   header: {
     logo: {
-      alt: '',
-      light: '',
-      dark: ''
+      alt: "",
+      light: "",
+      dark: "",
     },
     search: true,
     colorMode: true,
-    links: [{
-      'icon': 'i-simple-icons-github',
-      'to': 'https://github.com/AINNATE-TTS/tts-docs',
-      'target': '_blank',
-      'aria-label': 'TTSOpenAI API docs on GitHub'
-    }]
+    links: [
+      {
+        icon: "i-simple-icons-github",
+        to: "https://github.com/AINNATE-TTS/tts-docs",
+        target: "_blank",
+        "aria-label": "TTSOpenAI API docs on GitHub",
+      },
+    ],
   },
   footer: {
-    credits: 'Copyright © ' + new Date().getFullYear(),
+    credits: "Copyright © " + new Date().getFullYear(),
     colorMode: false,
-    links: [{
-      'icon': 'i-mingcute-voice-line',
-      'to': 'https://ttsopenai.com/',
-      'target': '_blank',
-      'aria-label': 'Our website'
-    }, {
-      'icon': 'i-openmoji-youtube',
-      'to': 'https://www.youtube.com/@TTSOPENAI86',
-      'target': '_blank',
-      'aria-label': 'YouTube'
-    }, {
-      'icon': 'i-openmoji-facebook',
-      'to': 'https://www.facebook.com/profile.php?id=61556251485523',
-      'target': '_blank',
-      'aria-label': 'Facebook'
-    }, {
-      'icon': 'i-simple-icons-github',
-      'to': 'https://github.com/AINNATE-TTS',
-      'target': '_blank',
-      'aria-label': 'GitHub'
-    }]
+    links: [
+      {
+        icon: "i-mingcute-voice-line",
+        to: "https://ttsopenai.com/",
+        target: "_blank",
+        "aria-label": "Our website",
+      },
+      {
+        icon: "i-openmoji-youtube",
+        to: "https://www.youtube.com/@TTSOPENAI86",
+        target: "_blank",
+        "aria-label": "YouTube",
+      },
+      {
+        icon: "i-openmoji-facebook",
+        to: "https://www.facebook.com/profile.php?id=61556251485523",
+        target: "_blank",
+        "aria-label": "Facebook",
+      },
+      {
+        icon: "i-simple-icons-github",
+        to: "https://github.com/AINNATE-TTS",
+        target: "_blank",
+        "aria-label": "GitHub",
+      },
+    ],
   },
   toc: {
-    title: 'Table of Contents',
+    title: "Table of Contents",
     bottom: {
-      title: 'Community',
-      edit: 'https://github.com/AINNATE-TTS/tts-docs/edit/main/content',
-      links: [{
-        icon: 'i-heroicons-star',
-        label: 'Star on GitHub',
-        to: 'https://github.com/nuxt/ui',
-        target: '_blank'
-      }, {
-        icon: 'i-heroicons-book-open',
-        label: 'Nuxt UI Pro docs',
-        to: 'https://ui.nuxt.com/pro/guide',
-        target: '_blank'
-      }, {
-        icon: 'i-simple-icons-nuxtdotjs',
-        label: 'Purchase a license',
-        to: 'https://ui.nuxt.com/pro/purchase',
-        target: '_blank'
-      }]
-    }
-  }
-})
+      title: "Community",
+      edit: "https://github.com/AINNATE-TTS/tts-docs/edit/develop/content",
+      links: [
+        {
+          icon: "i-mingcute-voice-line",
+          to: "https://ttsopenai.com/",
+          target: "_blank",
+          label: "Our website",
+        },
+        {
+          icon: "i-openmoji-youtube",
+          to: "https://www.youtube.com/@TTSOPENAI86",
+          target: "_blank",
+          label: "Follow on YouTube",
+        },
+        {
+          icon: "i-openmoji-facebook",
+          to: "https://www.facebook.com/profile.php?id=61556251485523",
+          target: "_blank",
+          label: "Follow on Facebook",
+        },
+      ],
+    },
+  },
+});
